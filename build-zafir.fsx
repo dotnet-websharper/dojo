ba204c4254c47b98eb07a8bba888d06de2288320
jankoa <andras.janko@gmail.com>
2016-06-03 11:28:10 +0200

    add Zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,52 @@
+﻿#load "tools/includes.fsx"
+
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.Dojo")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let library =
+    bt.Zafir.Extension("WebSharper.Dojo")
+        .SourcesFromProject()
+        .Embed(["dojo-config.js"])
+
+let typeProvider =
+    bt.Zafir.Library("WebSharper.Dojo.Provider")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.Project library
+                r.Assembly "System.Xml"
+                r.Assembly "System.Xml.Linq"
+            ])
+
+let tests =
+    bt.Zafir.HtmlWebsite("WebSharper.Dojo.Tests")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.Project library
+                r.Project typeProvider
+                r.NuGet("Zafir.Html").Latest(true).ForceFoundVersion().Reference()
+            ])
+
+bt.Solution [
+    library
+    typeProvider
+    tests
+
+    bt.NuGet.CreatePackage()
+        .Description("WebSharper extension for Dojo Toolkit")
+        .Add(library)
+        .Add(typeProvider)
+        .Configure(fun c ->
+            { c with
+                Authors = ["IntelliFactory"]
+                Title = Some "Zafir.Dojo"
+            }
+        )
+]
+|> bt.Dispatch
