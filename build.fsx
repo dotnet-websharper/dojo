2bb58c440a1e5afd5f33a8352690261e8193a651
Loic Denuziere <loic@denuziere.net>
2018-01-22 13:16:11 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,68 +1,17 @@
-#load "tools/includes.fsx"
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-open IntelliFactory.Build
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let bt =
-    BuildTool().PackageId("WebSharper.Dojo")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-let library =
-    bt.WebSharper.Extension("WebSharper.Dojo")
-        .SourcesFromProject()
-        .Embed(["dojo-config.js"])
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-do
-    use wc = new System.Net.WebClient()
-    let files = 
-        [|
-            "ProvidedTypes.fsi"
-            "ProvidedTypes.fs"
-            "AssemblyReader.fs"
-            "AssemblyReaderReflection.fs"
-            "ProvidedTypesContext.fs"
-        |]
-    for f in files do
-        wc.DownloadFile(
-            "https://raw.githubusercontent.com/fsprojects/FSharp.TypeProviders.StarterPack/master/src/" + f,
-            System.IO.Path.Combine(__SOURCE_DIRECTORY__, "WebSharper.Dojo.Provider", f)
-        )
-
-let typeProvider =
-    bt.WebSharper.Library("WebSharper.Dojo.Provider")
-        .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.Project library
-                r.Assembly "System.Xml"
-                r.Assembly "System.Xml.Linq"
-            ])
-
-let tests =
-    bt.WebSharper.HtmlWebsite("WebSharper.Dojo.Tests")
-        .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.Project library
-                r.Project typeProvider
-                r.NuGet("WebSharper.Html").Version("(,4.0)").Reference()
-            ])
-
-bt.Solution [
-    library
-    typeProvider
-    tests
-
-    bt.NuGet.CreatePackage()
-        .Description("WebSharper extension for Dojo Toolkit")
-        .Add(library)
-        .Add(typeProvider)
-        .Configure(fun c ->
-            { c with
-                Authors = ["IntelliFactory"]
-                Title = Some "WebSharper.Dojo"
-            }
-        )
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
